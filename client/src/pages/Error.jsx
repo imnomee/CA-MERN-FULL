af1604872352db8ff1f1a1f543ce220ac2fa9c37
Noman Rafiq <heyimnomee@gmail.com>
2024-02-26 00:52:26 +0500

    register page done

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,28 @@
 import { Link, useRouteError } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
+import img from '../assets/images/not-found.svg';
 
 const Error = () => {
     const error = useRouteError();
     console.log(error);
-
+    if (error.status === 404) {
+        return (
+            <Wrapper>
+                <div>
+                    <img src={img} alt="not-found-img" />
+                    <h3>Ohhh!!! Page not found...</h3>
+                    <p>
+                        We can&apos;t seem to find the page you are looking for.
+                    </p>
+                    <Link to="/dashboard">-= Back to Home =-</Link>
+                </div>
+            </Wrapper>
+        );
+    }
     return (
-        <div>
-            <h1>Error Page</h1>
-            <Link to="/">back to Home</Link>
-        </div>
+        <Wrapper>
+            <h3>Something went wrong...</h3>
+        </Wrapper>
     );
 };
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,45 @@
-import { Link } from 'react-router-dom';
-
+import { Form, Link } from 'react-router-dom';
+import { Logo } from '../components';
+import Wrapper from '../assets/wrappers/RegisterAndLoginPage';
+import { FormRow } from '../components/index';
 const Register = () => {
     return (
-        <div>
-            <h1>Register Page</h1>
-            <Link to="/login">Login</Link>
-        </div>
+        <Wrapper>
+            <form className="form">
+                <Logo />
+                <h4>Register</h4>
+                <FormRow
+                    defaultValue="john"
+                    labelText="name"
+                    name="name"
+                    type="text"
+                />
+                <FormRow
+                    defaultValue="doe"
+                    labelText="last name"
+                    name="lastName"
+                    type="text"
+                />
+                <FormRow type="text" name="location" defaultValue="earth" />
+                <FormRow
+                    type="email"
+                    name="email"
+                    defaultValue="example@email.com"
+                />
+                <FormRow
+                    type="password"
+                    name="password"
+                    defaultValue="secret123"
+                />
+                <button type="submit" className="btn btn-block">
+                    submit
+                </button>
+                <p>Already a member?</p>
+                <Link to="/login" className="member-btn">
+                    Login
+                </Link>
+            </form>
+        </Wrapper>
     );
 };
 
