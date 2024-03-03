eb94cb44f91df0ee73e2703ecfff20b811924807
Noman Rafiq <heyimnomee@gmail.com>
2024-03-03 18:06:46 +0500

    validate job inputs

--- middlewares/validation.middleware.js
+++ middlewares/validation.middleware.js
@@ -1,5 +1,6 @@
 import { body, validationResult } from 'express-validator';
 import { BadRequestError } from '../errors/custom.error.js';
+import { JOB_STATUS, JOB_TYPE } from '../utils/constants.js';
 
 const withValidationErrors = (validateValues) => {
     return [
@@ -14,6 +15,18 @@ const withValidationErrors = (validateValues) => {
         },
     ];
 };
+
+export const validateJobInput = withValidationErrors([
+    body('company').notEmpty().withMessage('company name is required'),
+    body('position').notEmpty().withMessage('position is required'),
+    body('jobLocation').notEmpty().withMessage('location is required'),
+    body('jobStatus')
+        .isIn(Object.values(JOB_STATUS))
+        .withMessage('invalid status value'),
+    body('jobType')
+        .isIn(Object.values(JOB_TYPE))
+        .withMessage('invalid job type'),
+]);
 
 export const validateTest = withValidationErrors([
     body('name')
--- routes/job.router.js
+++ routes/job.router.js
@@ -8,8 +8,13 @@ import {
     deleteAJob,
     createAJob,
 } from '../controllers/job.controller.js';
+import { validateJobInput } from '../middlewares/validation.middleware.js';
 
-router.route('/').get(getAllJobs).post(createAJob);
-router.route('/:id').get(getAJob).patch(patchAJob).delete(deleteAJob);
+router.route('/').get(getAllJobs).post(validateJobInput, createAJob);
+router
+    .route('/:id')
+    .get(getAJob)
+    .patch(validateJobInput, patchAJob)
+    .delete(deleteAJob);
 
 export default router;
